58e23a22ac97dc5e8c583ec93a49eb9fcd57b6b2
1dina <dinafadel69@gmail.com>
2024-09-21 15:38:42 +0300

    remove unused imports

--- app/src/main/java/com/example/speedotransfer/ui/screens/dashboard/components/more/FavouriteScreen.kt
+++ app/src/main/java/com/example/speedotransfer/ui/screens/dashboard/components/more/FavouriteScreen.kt
@@ -35,7 +35,6 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Color.Companion.Unspecified
 import androidx.compose.ui.graphics.Color.Companion.White
 import androidx.compose.ui.platform.LocalContext
