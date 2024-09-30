11b5c7b59db587287faf511e8d010d69062aa135
1dina <dinafadel69@gmail.com>
2024-09-30 18:50:51 +0300

    add header to interceptor and deal with session expired

--- app/src/main/java/com/example/speedotransfer/data/source/remote/retrofit/BankingAPICallable.kt
+++ app/src/main/java/com/example/speedotransfer/data/source/remote/retrofit/BankingAPICallable.kt
@@ -20,7 +20,6 @@ import retrofit2.Response
 import retrofit2.http.Body
 import retrofit2.http.DELETE
 import retrofit2.http.GET
-import retrofit2.http.Header
 import retrofit2.http.POST
 import retrofit2.http.PUT
 import retrofit2.http.Path
